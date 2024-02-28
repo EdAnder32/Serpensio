a3aeee312b4f0a575b02677e52a62cf72e1929ef
edalexan <edalexan@linux.myguest.virtualbox.org>
2024-02-28 21:47:24 +0100

    header.h ADICIONADO V. 1

---
+++ srcs/header.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edalexan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 20:13:47 by edalexan          #+#    #+#             */
+/*   Updated: 2024/02/28 20:33:41 by edalexan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef HEADER_H
+#define HEADER_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putstr(char *str);
+void	nome(char *str);
+void	limpar_tela(void);
+void	login(void);
+
+#endif
