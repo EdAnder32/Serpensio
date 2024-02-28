73ee4a2d58874667a2124b8b5b0d73a952603afc
edalexan <edalexan@linux.myguest.virtualbox.org>
2024-02-28 21:48:03 +0100

    tela_do_login.c ADICIONADO V. 2

--- srcs/tela_do_login.c
+++ srcs/tela_do_login.c
@@ -6,25 +6,37 @@
 /*   By: edalexan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 17:56:37 by edalexan          #+#    #+#             */
-/*   Updated: 2024/02/28 18:03:06 by edalexan         ###   ########.fr       */
+/*   Updated: 2024/02/28 21:27:33 by edalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include "header.h"
 
 void	limpar_tela(void)
 {
 	system ("clear");
 }
 
-void	login(void)
+int	main(void)
 {
+	char	nome[20];
+
+	/*tela do login*/
 	limpar_tela();
-	write (1, "-------------------\n", 20);
-	write (1, "-----SERPENSIO-----\n", 20);
-	write (1, "-------------------\n", 20);
-	write (1, "\n", 1);
-	write (1, "\n", 1);
-	nome_do_jogador();
+        write (1, "-------------------\n", 20);
+        write (1, "-----SERPENSIO-----\n", 20);
+        write (1, "-------------------\n", 20);
+        write (1, "\n", 1);
+        write (1, "\n", 1);
+        write (1, "Insira seu nome de jogador pra continuar: \n", 43);
+        fgets (nome, sizeof(nome), stdin);
+	limpar_tela();
+        printf ("Bem Vindo ao Serpensio %s\n", nome);
+	printf ("!! PRESSIONE QUALQUER TECLA PRA CONTINUAR \n");
+	getchar();
+        limpar_tela();
+	return (0);
 }
